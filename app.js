55d8dfc7d9da9bf2c81a57aa057ea8f52839387f
Johan <Javrangel014@gmail.com>
2025-02-03 11:58:45 -0400

    Examen

--- app.js
+++ app.js
@@ -32,6 +32,33 @@ const tasks = [
 ] 
 
 //Declaracion de Rutas
+//Ruta para crear una nueva tarea
+app.post('/tasks',(req,res) => {
+    const nuevaTarea = {
+        id: tasks.length + 1,
+        title: req.body.title,
+        description: req.body.description,
+        complete: (req.body.complete === 'true')
+    };
+//Una vez creada la nueva Tarea, se empuja al array principal
+    tasks.push(nuevaTarea);
+    res.send(nuevaTarea);
+    const output= {
+        code:200,
+        message:"Si lees este mensaje, esta linea funciona"
+     }
+     return res.send(output).status(200);
+})
+//Ruta para obtener la lista de tareas
+app.get('/tasks',(req,res) => {
+    res.send(tasks);
+    const output= {
+        code:200,
+        message:"Si lees este mensaje, esta linea funciona"
+     }
+     return res.send(output).status(200);
+    
+})
 //Ruta Principal del Servidor
 app.get('/',(req,res)=> {
     res.send('Tareas del Sistema');
@@ -42,16 +69,7 @@ app.get('/',(req,res)=> {
      }
      return res.send(output).status(200)
 })
-//Ruta para obtener la lista de tareas
-app.get('/tasks',(req,res) => {
-    res.send(tasks);
-    const output= {
-        code:200,
-        message:"Si lees este mensaje, esta linea funciona"
-     }
-     return res.send(output).status(200)
-    
-})
+
 //Ruta para Obtener una Tarea especifica
 app.get('/tasks/:id',(req,res) => {
     const tarea = tasks.find(c=> c.id === parseInt(req.params.id));
@@ -65,27 +83,22 @@ app.get('/tasks/:id',(req,res) => {
      }
      return res.send(output).status(200)
 })
-//Ruta para crear una nueva tarea
-app.post('/tasks',(req,res) => {
-    const tarea = {
-        id: tasks.length + 1,
+
+//Ruta para Actualizar Informacion
+app.put('/tasks/:id',(req,res) => {
+  const taskId = parseInt(req.params.id);
+  const updatedTask = {
         title: req.body.title,
         description: req.body.description,
         complete: (req.body.complete === 'true')
-    };
-//Una vez creada la nueva Tarea, se empuja al array principal
-    tasks.push(tarea);
-    res.send(tarea);
-
-    const output= {
-        code:200,
-        message:"Si lees este mensaje, esta linea funciona"
-     }
-     return res.send(output).status(200)
-    
-})
-//asdsasdasa
-app.put('/tasks/:id',(req,res) => {
+  };
+  const existingTaskIndex = tasks.findIndex((t) => t.id === taskId);
+  if (existingTaskIndex === -1) {
+    res.status(404).send({ error: 'Tarea no encontrada' });
+  } else {
+    tasks[existingTaskIndex] = { ...tasks[existingTaskIndex], ...updatedTask };
+    res.send(tasks[existingTaskIndex]);
+  }
     const output= {
         code:200,
         message:"Si lees este mensaje, esta linea funciona"
@@ -96,6 +109,12 @@ app.put('/tasks/:id',(req,res) => {
 })
 //Ruta para Borrar una Tarea especifica
 app.delete('/tasks/:id',(req,res) => {
+    const tarea = tasks.find(c => c.id === parseInt(req.params.id));
+    if(!tarea) return res.status(404).send('Tarea no Encontrada');
+
+    const index = tasks.indexOf(tarea);
+    tasks.splice(index, 1);
+    res.send(tarea);
     const output= {
         code:200,
         message:"Si lees este mensaje, esta linea funciona"
